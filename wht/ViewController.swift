5539c1faf83790da5cdfcf41bcac339d08010821
Edward <ekundrat@gmail.com>
2016-09-23 16:14:54 -0400

    added files for tits

--- wht/ViewController.swift
+++ wht/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tits: UIImageView!
 
+    @IBOutlet weak var tittybutton: UIButton!
+    @IBOutlet weak var tittybuttontwo: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func titpress(sender: AnyObject) {
+        tits.hidden=true
+        tittybutton.hidden=true
+        tittybuttontwo.hidden=false
+    }
+    @IBAction func titpresstwo(sender: AnyObject) {
+        tits.hidden=false
+        tittybutton.hidden=false
+        tittybuttontwo.hidden=true
+    }
 
 }
 
